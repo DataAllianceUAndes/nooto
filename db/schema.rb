25a6a8fb5bc6d95e9527aacbd8a67e9f3b59007e
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2022-10-02 19:01:27 -0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_01_232904) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_02_215859) do
   create_table "classnotes", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.integer "course_id", null: false
+    t.index ["course_id"], name: "index_classnotes_on_course_id"
+    t.index ["user_id"], name: "index_classnotes_on_user_id"
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.integer "classnote_id", null: false
+    t.index ["classnote_id"], name: "index_courses_on_classnote_id"
+    t.index ["user_id"], name: "index_courses_on_user_id"
+  end
+
+  create_table "enrollments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.integer "course_id", null: false
+    t.index ["course_id"], name: "index_enrollments_on_course_id"
+    t.index ["user_id"], name: "index_enrollments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +56,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_01_232904) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "classnotes", "courses"
+  add_foreign_key "classnotes", "users"
+  add_foreign_key "courses", "classnotes"
+  add_foreign_key "courses", "users"
+  add_foreign_key "enrollments", "courses"
+  add_foreign_key "enrollments", "users"
 end
