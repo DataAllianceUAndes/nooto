4b9cf9c7883a74c22c1eb707aed458ea7adf24ea
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2022-10-01 23:14:20 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :classnotes
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' },
+                      path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'classnotes#index'
 end
